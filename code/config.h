d00480f23d530868f5cfe8272d4a438010a23eb1
oleg134514 <168273249+oleg134514@users.noreply.github.com>
2026-06-29 19:45:40 +0500

    Update config.h

--- code/config.h
+++ code/config.h
@@ -2,14 +2,14 @@
 #pragma once
 
 // --- Wi-Fi Settings ---
-const char* WIFI_SSID = "WL24";
-const char* WIFI_PASS = "12H>*5x-g1?q8g0^I!R_J%F%CyCih_v-^3BYj=u5NV<O=b!8I^clj2WLS=Gjaa";
+const char* WIFI_SSID = "WIFI SSID";
+const char* WIFI_PASS = "WIFI PASS";
 
 // --- Kodi Settings ---
-const char* KODI_IP = "172.16.76.126";
+const char* KODI_IP = "192.168.0.100";
 const int KODI_PORT = 8080;
-const char* KODI_USER = "kodi";
-const char* KODI_PASS = "kodi";
+const char* KODI_USER = "KODI USER";
+const char* KODI_PASS = "KODI PASS";
 
 // --- Pins ---
 #define IR_RECEIVER_PIN 4
@@ -22,5 +22,5 @@ const char* KODI_PASS = "kodi";
 // ============================================================================
 // NTP НАСТРОЙКИ
 // ============================================================================
-const char* NTP_SERVER = "172.16.76.59";            // NTP сервер
+const char* NTP_SERVER = "NTP SERVER IP";            // NTP сервер
 const int NTP_TIMEZONE = 5;                         // Часовой пояс (например, 5 для UTC+5, -3 для UTC-3)
